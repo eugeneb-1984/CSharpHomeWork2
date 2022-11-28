27ffd3b0a20b358d7ed90255927efa9db0bd923c
Eugene Bogdanov <eugeneb1984@gmail.com>
2022-11-28 21:42:23 +0300

    Adding Task1 code

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное целое число: ");
+string input = Console.ReadLine();
+bool isNumber = Int32.TryParse (input, out int number);
+if (isNumber && number > 99 && number < 1000) {
+    int secondDigit = number / 10 % 10;
+    Console.WriteLine($"Вторая цифра числа {number}: {secondDigit}");
+}
+else {
+    Console.WriteLine("Вы ошиблись при вводе. Введите трёхзначное целое число.");
+}
